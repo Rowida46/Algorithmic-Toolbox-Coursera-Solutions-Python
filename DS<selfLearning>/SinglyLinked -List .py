44d03bdb04f4f3b1bcd51913fb25cb2dfd473bb7
Rowida <52299389+Rowida46@users.noreply.github.com>
2020-02-02 00:43:37 +0200

    Update SinglyLinked -List .py

--- DS<selfLearning>/SinglyLinked -List .py
+++ DS<selfLearning>/SinglyLinked -List .py
@@ -40,4 +40,56 @@ s.push(4)
 print(s.popAT0())
 
 print(s.head.element)
+#---------------------------------------------------------------------
 
+
+
+# <<<<<Implementing a Stack with a Singly Linked List >>>>
+
+class LinkedListQue:
+     # <<<InnerClass>>
+    class Node :
+        __solts__= 'element'  ,  'next' 
+        def __init__(self ,element,  Next ):
+            self.next = Next
+            self.element = element
+    def __init__(self):
+        self.head =None
+        self.tail = None
+        self.size =0
+    def __len__(self):
+        return self.size
+    def isEmpty(self):
+        return self.size ==0
+    def peek(self):
+        return self.head.element
+    def deQueue(self):
+        ### to remove& return the first element
+        ans = self.head.element
+        self.head = self.head.next
+        self.size -=1
+        ### if queue is empty >>> gonna update tail to point to Null val
+        if self.isEmpty():
+            self.tail = None
+        return ans
+    def enQueue(self , e):
+        '''
+        to add an element to the <<BACK>> of  the Queue
+        '''
+        
+        # >>1 first create a new Node
+        newelement = self.Node(e, None)
+        '''
+         if the Queue is empty >> gonna set the new element as a head and tail as wll
+         otherwise 
+         if it's not empty  set tail point to the new element
+        '''     
+        if self.isEmpty():
+            self.head = newelement
+        else :
+            self.tail.next = newelement
+        self.size+=1
+        self.tail == newelement
+        
+        
+            

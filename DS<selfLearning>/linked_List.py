d5b69af3b54108863d8e50f4764cc096cea0697e
Rowida <52299389+Rowida46@users.noreply.github.com>
2022-03-06 00:57:16 +0200

    Update linked_List.py

--- DS<selfLearning>/linked_List.py
+++ DS<selfLearning>/linked_List.py
@@ -1,150 +1,156 @@
-class Node :
-    def __init__(self , data):
-        self.data = data 
+class Node:
+    def __init__(self, data):
+        self.data = data
         self.next = None
-        
-        
+
 
 class linkedList:
     def __init__(self):
-        self.tail = None 
+        self.tail = None
         self.head = None
-### to <<<<< add >>>>
-    def pushfront(self , key):
+
+    ### to <<<<< add >>>>
+    def pushfront(self, key):
         node = Node(key)
-        if not self.head : ### it's just an empty linkedlist
-            self.head = self.tail =  node
-        else :
-            node.next = self.head 
-            self.head = node
-            
-    def pushback(self , key):
-        node = Node(key)
-        if not self.head :
+        if not self.head:  ### it's just an empty linkedlist
             self.head = self.tail = node
-        else :
+        else:
+            node.next = self.head
+            self.head = node
+
+    def pushback(self, key):
+        node = Node(key)
+        if not self.head:
+            self.head = self.tail = node
+        else:
             self.tail.next = node
             self.tail = node
-    def insertbetween(self , data , index):
+
+    def insertbetween(self, data, index):
         node = Node(data)
-        tmp = self.head 
+        tmp = self.head
         i = 1
         while tmp:
-            if i == index :
+            if i == index:
                 node.next = tmp.next
                 tmp.next = node
                 break
             tmp = tmp.next
             i += 1
-            
-    def CountRec(self, node): 
-        if (not node): 
+
+    def CountRec(self, node):
+        if not node:
             return 0
-        else: 
-            return 1 + self.CountRec(node.next) 
-  
-    # A wrapper over getCountRec() 
-    def getCount(self): 
-        return self.getCountRec(self.head) 
-##### <<<< to delet >>>>
+        else:
+            return 1 + self.CountRec(node.next)
+
+    # A wrapper over getCountRec()
+    def getCount(self):
+        return self.getCountRec(self.head)
+
+    ##### <<<< to delet >>>>
     def popfront(self):
-        if not self.head :
-            return False 
+        if not self.head:
+            return False
         val = self.head.data
-        if self.head == self.tail :
+        if self.head == self.tail:
             self.tail = self.head = None
-        else : 
+        else:
             self.head = self.head.next
         return val
-            
+
     def popback(self):
-        if not self.head :
+        if not self.head:
             return False
-        if self.head == self.tail : ### there is only one element in the list
+        if self.head == self.tail:  ### there is only one element in the list
             self.head = self.tail = None
-        else :
+        else:
             curr = self.head
-            while curr.nextnext :
+            while curr.nextnext:
                 curr = curr.next
             curr.next = None
             self.tail = curr
-            
-            
-    def popdata(self , e ):
-        tmp = self.head       
+
+    def popdata(self, e):
+        tmp = self.head
         ## if we gonna delet the head
-        if self.head.data == e :
+        if self.head.data == e:
             self.head = tmp.next
-            return 
-        while tmp :
-            if tmp.next.data == e :
+            return
+        while tmp:
+            if tmp.next.data == e:
                 tmp.next = tmp.next.next
                 break
             tmp = tmp.next
-    def deletindex(self , position):
-        i = 1 
+
+    def deletindex(self, position):
+        i = 1
         tmp = self.head
-        if position == 0 :
+        if position == 0:
             if self.head.next:
                 self.head = tmp.next
-                return 
+                return
             self.head = None
 
-        while tmp :
+        while tmp:
             if i == position:
                 tmp.next = tmp.next.next
                 break
             tmp = tmp.next
             i += 1
-    def erase(self , key):
-        if not self.head :
-            return False
-        node = Node(key)
-        curr = self.head 
-        while curr.next :
-            if curr.next.data == node.data :
-                curr.next = curr.next.next
-                return 
-            curr = curr.next
-#####################################################    
-    def isEmpty(self):
-        return (self.tail and self.head) == None
-    def topfront(self):
-        return self.head.data
-    def topback(self):
-        return self.tail.data
-   
-    def find(self , key):
-        if not self.head :
+
+    def erase(self, key):
+        if not self.head:
             return False
         node = Node(key)
         curr = self.head
-        while curr :
-            if curr.data == node.data :
+        while curr.next:
+            if curr.next.data == node.data:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+    #####################################################
+    def isEmpty(self):
+        return (self.tail and self.head) == None
+
+    def topfront(self):
+        return self.head.data
+
+    def topback(self):
+        return self.tail.data
+
+    def find(self, key):
+        if not self.head:
+            return False
+        node = Node(key)
+        curr = self.head
+        while curr:
+            if curr.data == node.data:
                 return node.data
             curr = curr.next
         return -1
+
     def reverse_iterative(self):
         ### firstly ,  we gonna make head point to NONE and keep track to thenext node
-        cur = self.head 
+        cur = self.head
         next_node = head.next
         cur.next = None
-        while next_node :
-        ### our strategy is to save the next_node and play woth the current pointer
+        while next_node:
+            ### our strategy is to save the next_node and play woth the current pointer
             tmp = next_node.next
-            next_node.next = cur ### to make next_node point to the pre node
+            next_node.next = cur  ### to make next_node point to the pre node
             head = cur = next_node
             next_node = tmp
-    
+
     def tostring(self):
-        cur = self.head 
-        i = 0 
-        while cur !=None :
-            print(i , cur.data)
+        cur = self.head
+        i = 0
+        while cur != None:
+            print(i, cur.data)
             cur = cur.Next
-            i+=1
-   
-    
+            i += 1
+
     def getnode(self):
         tmp = self.head
         while tmp:
